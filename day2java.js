c51549822f24b94a9296bd5e647235232c0d172c
mdrahmatullahamn <rmd831219@gmail.com>
2023-02-10 16:48:20 +0530

    java

---
+++ day2java.js
@@ -0,0 +1,41 @@
+console.log("at middle")
+var number1=10;
+var number2=20
+if(number2>number1)
+console.log("number2 is a grater than number1");
+else{
+    console.log("for false")
+}
+console.log("at middle")
+var number3=10
+var number4=20
+console.log("at end")
+var userAge=18
+if(userAge>18){
+    console.log("user is ablible!")
+} else if(userAge==18){
+    console.log("user is applicable for lerning" )
+}
+else{
+    console.log ("user is not applicable!")
+}
+var usernumber =88738297896;
+if ((usernumber % 2)==0){
+    console.log("provide number is even")
+}else{
+    console.log("provide number is odd")
+}
+var expr ='apple';
+switch (expr){
+    case 'orange':
+    console.log("orange...")
+    break;
+    case 'apple':
+    console.log("apple")
+    break;
+    default: console.log (`sorry,we are not of ${expr}.`);
+}
+var myName ="md amaan"
+console.log("my name is",myName, "and iam from bihar")
+console.log(`my name is ${myName} and im from bihar`)
+
---
+++ javascript-js.js
@@ -0,0 +1,12 @@
+// var myname ='amaan';
+// console.log(myname);
+// console.log(!myname);
+// console.log(!!myname)
+a=true;
+b=false;
+console.log(a || b);
+console.log(b || a);
+console.log(a && b);
+console.log(!(a && b));
+console.log(!!(a && b));
+
