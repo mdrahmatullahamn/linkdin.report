7822720aa04e9cb171d3bc3ded1e51ce58d6f930
mdrahmatullahamn <rmd831219@gmail.com>
2023-02-25 19:35:28 +0530

    update

---
+++ day20.js
@@ -0,0 +1,29 @@
+// for(var i = 20; i <= 40; i++){
+//     if(i%2===0 )
+//     console.log(i)
+// }
+
+
+// var  count =0
+// for(var i = 20; i<=40; i++){
+//     if(i%2===0){
+//         count++;
+//     }
+// }
+// Q 6. print count of numbers form 147-254,which are dissible by 3 and odd
+
+// for (var i =147; i <= 254; i++){
+//     if (i%1==0){
+//         console.log(i)
+//     }
+// }
+// for (var i=1;  i<=100;i++){
+//     console.log(i) 
+
+let str = '45';
+
+for (let i = 0; i < 9; i++) {
+  str = str + i;
+}
+
+console.log(str);
---
+++ day21.js
@@ -0,0 +1,56 @@
+// Q.7 find count of number 45 from given Array
+
+// [12, 13, 45, 43,45, 76, 89, 54 , 10, 345,]
+
+// var count=0;
+// var array =[12, 13, 45, 43,45, 76, 89, 54,87,87,90,87,67,45,45]
+// for( var i=0 ; i< array.length; i++){
+//     if(array[i] ===45){
+//         count++;
+//     }
+// }
+// console.log (count)
+// Q .8 find numbers which addiation is target 
+// var nums=[11,15,7, 2, ];
+// var target =9;
+// for(var i =0; i < nums.length-1; i++){
+//     for(var j=i+1; j<nums.length ; j++){
+//         if(nums[i] + nums[j]=== target){
+//             console.log(nums[i],nums[j])
+                    
+//         }
+//     }
+// }
+// Q .9 find aidisston of every second number from 57689-346578 
+// var firstdigit=1
+// var seconddigit=10
+// var add=0
+// for(var i=57689+1 ;i<346578;i+=2){
+//     if(i%2===0){
+//         add = add + i;
+//     }
+// }
+// console.log(add, "add here");
+
+
+// var add=0
+// for (var i=45+1 ;i<100;i+=2){
+//     if(i%2===0){
+//         add =add +i
+//     }
+// }
+// console.log(add,"add here")
+
+// Q .10 find 3 number which addication is target
+//  var gArray =[2,4,6,9,10,13,78,12,34,23 ,32 ,1, 2]
+// var target=35
+// for (var i=0; i<gArray.length;i+=2 ){
+//     for(j=i+1;j<gArray.length;j++ ){
+//         for(k=j+1; k<gArray.length;k++){
+//             if(gArray[i] +gArray[j]+ gArray[k] ===target){
+//                 console.log(gArray[i],gArray[j],gArray[k])
+//             }
+//         }
+//     }
+// }
+
